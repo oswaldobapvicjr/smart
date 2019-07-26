e116a5da0d56d4436c77b4ed99079fc6a07d59c0
Oswaldo Baptista Vicente Junior <45291656+oswaldobapvicjr@users.noreply.github.com>
2019-07-26 19:30:40 -0300

    Server config externalized to properties file

--- src/main/java/net/obvj/smart/conf/SmartProperties.java
+++ src/main/java/net/obvj/smart/conf/SmartProperties.java
@@ -1,22 +1,27 @@
 package net.obvj.smart.conf;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class SmartProperties
 {
+    private static final Logger LOG = Logger.getLogger("smart-server");
 
     public static final String CONSOLE_PROMPT = "console.prompt";
     private static final String CONSOLE_PROMPT_DEFAULT = "smart>";
 
     public static final String CLASSIC_CONSOLE_ENABLED = "classic.console.enabled";
     private static final String CLASSIC_CONSOLE_ENABLED_DEFAULT = "true";
-    
+
     public static final String CLASSIC_CONSOLE_PORT = "classic.console.port";
     private static final String CLASSIC_CONSOLE_PORT_DEFAULT = "1910";
 
     public static final String CLASSIC_CONSOLE_SESSION_TIMEOUT_SECONDS = "classic.console.session.timeout.seconds";
     private static final String CLASSIC_CONSOLE_SESSION_TIMEOUT_SECONDS_DEFAULT = "60";
-    
+
     private static final Properties defaults = new Properties();
     static
     {
@@ -32,6 +37,27 @@ public class SmartProperties
 
     private SmartProperties()
     {
+        readSmartPropertiesFile();
+    }
+
+    private void readSmartPropertiesFile()
+    {
+        LOG.info("Searching for smart.properties file...");
+        try (final InputStream stream = SmartProperties.class.getClassLoader().getResourceAsStream("smart.properties"))
+        {
+            properties.load(stream);
+            LOG.info("smart.properties loaded successfully");
+        }
+        catch (NullPointerException e)
+        {
+            LOG.warning("smart.properties not found. Using default properties...");
+        }
+        catch (IOException e)
+        {
+            LOG.log(Level.WARNING,
+                    "Unable to read smart.properties file. Using default properties. Error details: {0} ({1})",
+                    new String[] { e.getClass().getName(), e.getLocalizedMessage() });
+        }
     }
 
     public static SmartProperties getInstance()
@@ -43,12 +69,12 @@ public class SmartProperties
     {
         return properties.getProperty(key, defaults.getProperty(key));
     }
-    
+
     public int getIntProperty(String key)
     {
         return Integer.parseInt(getProperty(key));
     }
-    
+
     public boolean getBooleanProperty(String key)
     {
         return Boolean.parseBoolean(getProperty(key));
