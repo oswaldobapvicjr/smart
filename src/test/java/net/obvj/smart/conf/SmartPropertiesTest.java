c3492c1b60118448b1aef857dfd157501dc1330e
Oswaldo Baptista Vicente Junior <45291656+oswaldobapvicjr@users.noreply.github.com>
2019-09-06 01:29:24 -0300

    A test with the SmartProperties class

--- src/test/java/net/obvj/smart/conf/SmartPropertiesTest.java
+++ src/test/java/net/obvj/smart/conf/SmartPropertiesTest.java
@@ -1,9 +1,16 @@
 package net.obvj.smart.conf;
 
 import static org.junit.Assert.*;
+
+import java.io.IOException;
+
 import static net.obvj.smart.conf.SmartProperties.*;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 /**
  * Unit tests for the {@link SmartProperties} class.
@@ -11,6 +18,8 @@ import org.junit.Test;
  * @author oswaldo.bapvic.jr
  * @since 2.0
  */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(SmartProperties.class)
 public class SmartPropertiesTest
 {
     private static final String UNKNOWN_FILE = "unknownFile";
@@ -34,6 +43,22 @@ public class SmartPropertiesTest
     public void testDefaultConsolePrompt()
     {
         SmartProperties smart = new SmartProperties(UNKNOWN_FILE);
+        assertEquals(0, smart.properties.size());
+        assertEquals(CONSOLE_PROMPT_DEFAULT, smart.getProperty(CONSOLE_PROMPT));
+    }
+
+    /**
+     * Tests that the default value for a string property is retrieved in case of an
+     * IOException reading the properties file
+     */
+    @Test
+    public void testDefaultConsolePromptDueToIOException()
+    {
+        // Mocking only the readPropertiesFile method
+        PowerMockito.stub(PowerMockito.method(SmartProperties.class, "readPropertiesFile", String.class))
+                .toThrow(new IOException("test"));
+
+        SmartProperties smart = new SmartProperties("testProperties/smart-consolePrompt1.properties");
         assertEquals(0, smart.properties.size());
         assertEquals(CONSOLE_PROMPT_DEFAULT, smart.getProperty(CONSOLE_PROMPT));
     }
